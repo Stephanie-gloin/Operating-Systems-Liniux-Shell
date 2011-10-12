3d4e005e1eca5421e55d76b4576b2a5b76088bf2
ZachCook <zachary.cook@mycampus.uoit.ca>
2011-10-12 18:25:37 -0400

    I put my shell program into Shell.c, it has clr and quit functionality, and is documented.
    More functionality can be added by if statement checks of args (else
    if(strcmp(args[...)

--- src/Shell.c
+++ src/Shell.c
@@ -1,17 +1,73 @@
 /*
  ============================================================================
  Name        : Shell.c
- Author      : 
- Version     :
+ Author      : Zachary Cook, Stephanie Gloin, Darryl Barnhart
+ Version     : 0.01
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : Our Linux Shell Project for Operating Systems
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define BUFSIZE 1000
+#define PROMPT "==>"
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	//Create Buffers for IO
+	char input[BUFSIZE];
+	char commands[BUFSIZE];
+	char* args[BUFSIZE];
+
+	//run shell forever until told to exit
+	while(1){
+		//Put prompt to user and get their input
+		fputs(PROMPT,stdout);
+		fgets(input,BUFSIZE,stdin);
+
+		//Tokenize the users input
+
+		int i=0;
+		//Loop and store until no more tokens
+		while(i<1000){
+			char* temp;
+
+			if(i==0){
+				temp=strtok(input," \n\t");	//give initial array to tokenize and get first token
+			}else{
+				temp=strtok(NULL," \n\t");	//give NULL to take tokens from same input starting after previous token
+			}
+
+			if(temp==NULL){	//no more tokens
+				break;
+			}else{//add tokens to args
+				args[i]=temp;
+				i++;
+			}
+		}
+
+		//Do some checks on args to determine what to put in command buffer
+		if(strcmp(args[0],"clr")==0){
+			strcat(commands,"clear");
+		}
+		else if(strcmp(args[0],"quit")==0){
+			break;
+		}
+		//execute the commands specified
+		system(commands);
+
+		//clear buffers
+		strcpy(commands,"");
+		strcpy(input,"");
+		int j=0;
+		while(args[j]!=NULL){
+			strcpy(args[j],"");
+			j++;
+		}
+	}
+	fputs("Shell Terminated\n\r",stdout);
+	fflush(stdout);
 	return EXIT_SUCCESS;
 }
